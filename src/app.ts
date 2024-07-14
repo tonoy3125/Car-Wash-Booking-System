d25b8e0018a15419160ba56c00ca5f4276e9d1d0
tonoy3125 <shaifshajedt@gmail.com>
2024-07-14 17:55:55 +0600

    eslint and prettier setup

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,16 @@
-import express, { Application, Request, Response } from "express";
-import cors from "cors";
-import cookieParser from "cookie-parser";
-const app: Application = express();
-const port = 3000;
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+const app: Application = express()
+// const port = 3000
 
 // parser
-app.use(express.json());
-app.use(cookieParser());
-app.use(cors({ origin: ["http://localhost:5173"] }));
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors({ origin: ['http://localhost:5173'] }))
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
-});
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!')
+})
 
-export default app;
+export default app
--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,19 @@
-import mongoose from "mongoose";
-import config from "./app/config";
-import { Server } from "http";
-import app from "./app";
+import mongoose from 'mongoose'
+import config from './app/config'
+import { Server } from 'http'
+import app from './app'
 
-let server: Server;
+let server: Server
 
 async function main() {
   try {
-    await mongoose.connect(config.database_url as string);
+    await mongoose.connect(config.database_url as string)
     server = app.listen(config.port, () => {
-      console.log(`Example app listening on port ${config.port}`);
-    });
+      console.log(`Example app listening on port ${config.port}`)
+    })
   } catch (err) {
-    console.log(err);
+    console.log(err)
   }
 }
 
-main();
+main()
