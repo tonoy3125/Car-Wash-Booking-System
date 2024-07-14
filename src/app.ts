aea5213ec0adfb333e75be8cf4ddd21d2506d5a5
tonoy3125 <shaifshajedt@gmail.com>
2024-07-14 19:35:54 +0600

    not found route added and called

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
 import cookieParser from 'cookie-parser'
 import { routes } from './app/routes'
+import globalErrorHandler from './app/middlewares/globalErrorHandler'
+import notFound from './app/errors/notFound'
 const app: Application = express()
 // const port = 3000
 
@@ -13,8 +15,17 @@ app.use(cors({ origin: ['http://localhost:5173'] }))
 // Application Routes
 app.use('/api/v1', routes)
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!')
-})
+const getAController = (req: Request, res: Response) => {
+  const a = 10
+  res.send(a)
+}
+
+app.get('/', getAController)
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+app.use(globalErrorHandler)
+
+// Not found
+app.use(notFound)
 
 export default app
