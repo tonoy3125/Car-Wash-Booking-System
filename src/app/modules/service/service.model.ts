604326f8decde37a83456a389e786a83f435fd94
tonoy3125 <shaifshajedt@gmail.com>
2024-07-15 19:47:54 +0600

    service interface and model added

---
+++ src/app/modules/service/service.model.ts
@@ -0,0 +1,27 @@
+import { model, Schema } from 'mongoose'
+import { TService } from './service.interface'
+
+const serviceSchema = new Schema<TService>({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  duration: {
+    type: Number,
+    required: true,
+  },
+  isDeleted: {
+    type: Boolean,
+    default: false,
+  },
+})
+
+export const Service = model<TService>('Service', serviceSchema)
