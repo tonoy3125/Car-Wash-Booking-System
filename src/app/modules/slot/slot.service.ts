f9ccef7a3ba0715ced45609642d8cc3dfe170b9a
tonoy3125 <shaifshajedt@gmail.com>
2024-07-16 14:24:28 +0600

    service does not exists by this id method added

--- src/app/modules/slot/slot.service.ts
+++ src/app/modules/slot/slot.service.ts
@@ -1,7 +1,19 @@
+import httpStatus from 'http-status'
+import { AppError } from '../../errors/AppError'
+import { Service } from '../service/service.model'
 import { TSlot } from './slot.interface'
 import { Slot } from './slot.model'
 
 const createSlotIntoDB = async (payload: TSlot) => {
+  const service = await Service.findById(payload?.service)
+
+  if (!service) {
+    throw new AppError(
+      httpStatus.NOT_FOUND,
+      'Service does not exists by this id',
+    )
+  }
+
   const result = await Slot.create(payload)
   return result
 }
